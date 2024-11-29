60ef93a5d6de60446dc418151ad3e27a15a3e1e7
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-11-29 16:50:41 +0900

    레전드

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,2 +1,12 @@
 import React from 'react';
-import 
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                
+            </nav>
+        </header>
+    );
+}
